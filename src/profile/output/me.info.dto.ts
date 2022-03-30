fc11a52aa0c25ca0b4a149521ee1e3a70f487c1a
rkwadriga <dmitry.kushneriov@obreey-products.com>
2022-03-30 17:47:51 +0300

    Init user's email on "GET /profile" request

--- src/profile/output/me.info.dto.ts
+++ src/profile/output/me.info.dto.ts
@@ -1,7 +1,13 @@
 import {ProfileInfoDto} from "./profile.info.dto";
 import {Expose} from "class-transformer";
+import {User} from "../../auth/user.entity";
 
 export class MeInfoDto extends ProfileInfoDto {
     @Expose()
     email: string;
+
+    constructor(user: User) {
+        super(user);
+        this.email = user.email;
+    }
 }
