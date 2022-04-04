6dc5d134acc5642a5f6cda7e180ed6d9b4988060
rkwadriga <dmitry.kushneriov@obreey-products.com>
2022-04-04 18:23:12 +0300

    Add "birthday" field to "my profile" output

--- src/profile/output/me.info.dto.ts
+++ src/profile/output/me.info.dto.ts
@@ -7,6 +7,9 @@ export class MeInfoDto extends ProfileInfoDto {
     email: string;
 
     @Expose()
+    birthday: Date;
+
+    @Expose()
     imagesLimit: number;
 
     @Expose()
@@ -15,6 +18,7 @@ export class MeInfoDto extends ProfileInfoDto {
     constructor(user: User) {
         super(user);
         this.email = user.email;
+        this.birthday = user.profile?.birthday;
         this.imagesLimit = user.settings?.imagesLimit ?? Number(process.env.DEFAULT_USER_IMAGES_LIMIT);
         this.maximumImageSIze = user.settings?.maximumImageSIze ?? Number(process.env.DEFAULT_USER_MAX_IMAGE_SIZE);
     }
