e0a4b07fc2d849b540bd44dc1db64b299210d2bc
rkwadriga <dmitry.kushneriov@obreey-products.com>
2022-04-07 17:54:16 +0300

    Add "showGender" field to "GET /profile" request

--- src/profile/output/me.info.dto.ts
+++ src/profile/output/me.info.dto.ts
@@ -1,6 +1,7 @@
 import {ProfileInfoDto} from "./profile.info.dto";
 import {Expose} from "class-transformer";
 import {User} from "../../auth/user.entity";
+import {Gender} from "../profile.entity";
 
 export class MeInfoDto extends ProfileInfoDto {
     @Expose()
@@ -15,11 +16,15 @@ export class MeInfoDto extends ProfileInfoDto {
     @Expose()
     maximumImageSIze: number;
 
+    @Expose()
+    showGender: Gender;
+
     constructor(user: User) {
         super(user);
         this.email = user.email;
         this.birthday = user.profile?.birthday;
         this.imagesLimit = user.settings?.imagesLimit ?? Number(process.env.DEFAULT_USER_IMAGES_LIMIT);
         this.maximumImageSIze = user.settings?.maximumImageSIze ?? Number(process.env.DEFAULT_USER_MAX_IMAGE_SIZE);
+        this.showGender = user.settings?.showGender;
     }
 }
