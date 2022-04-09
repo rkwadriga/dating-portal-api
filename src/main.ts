242951a70cad687f17aa7d409b8ea3c821f766fe
rkwadriga <dmitry.kushneriov@obreey-products.com>
2022-04-09 15:47:38 +0300

    Set maximum request body to 250mb

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+  app.use(bodyParser.json({ limit: '250mb' }));
   await app.listen(3000);
 }
 bootstrap();
