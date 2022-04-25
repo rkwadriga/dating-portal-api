15376b7b646caa2445789026b3abb8abc47c49d2
rkwadriga <dmitry.kushneriov@obreey-products.com>
2022-04-25 11:06:13 +0300

    Return PairsController with API action "GET /pairs/list"

--- src/dating/dating.module.ts
+++ src/dating/dating.module.ts
@@ -1,22 +1,23 @@
-import {Module} from "@nestjs/common";
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {LocalStrategy} from "../auth/strategies/local.strategy";
-import {JwtStrategy} from "../auth/strategies/jwt.strategy";
-import {Photo} from "../profile/photo.entity";
-import {User} from "../auth/user.entity";
-import {ProfilesService} from "./profiles.service";
-import {ProfilesController} from "./profiles.controller";
-import {Profile} from "../profile/profile.entity";
-import {Settings} from "../profile/settings.entity";
-import {Contact} from "./contact.entity";
-import {DatingService} from "./dating.service";
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { LocalStrategy } from "../auth/strategies/local.strategy";
+import { JwtStrategy } from "../auth/strategies/jwt.strategy";
+import { Photo } from "../profile/photo.entity";
+import { User } from "../auth/user.entity";
+import { ProfilesService } from "./profiles.service";
+import { ProfilesController } from "./profiles.controller";
+import { Profile } from "../profile/profile.entity";
+import { Settings } from "../profile/settings.entity";
+import { Contact } from "./contact.entity";
+import { DatingService } from "./dating.service";
+import { PairsController } from "./pairs.controller";
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([User, Photo, Profile, Settings, Contact])
     ],
     providers: [LocalStrategy, JwtStrategy, ProfilesService, DatingService],
-    controllers: [ProfilesController]
+    controllers: [ProfilesController, PairsController]
 })
 export class DatingModule {
 
